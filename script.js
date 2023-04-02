ef7240b3ae7158525f929172764022e312d2600c
thisischinmaya <112348632+thisischinmaya@users.noreply.github.com>
2023-04-02 20:26:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navBar=document.querySelector('.navbar')
+
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x')
+   navBar.classList.toggle('active')
+}
